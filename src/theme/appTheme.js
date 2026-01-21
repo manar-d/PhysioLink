ec627d08b4d379d59daa8d685a75994fb500cf0e
manar-d <manar.alshiha@inspirejo.com>
2026-01-21 12:40:57 +0300

    refactor(theme): simplify MUI theme and update color palette

--- src/theme/appTheme.js
+++ src/theme/appTheme.js
@@ -1,78 +1,38 @@
 import { createTheme } from "@mui/material/styles";
 
 const baseTheme = {
-  
+  palette: {
+    primary: {
+      main: "#259687", //base color
+      light: "#4ebda9",
+      dark: "#1b786d",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#65afff", // secondary color
+      contrastText: "#000",
+    },
+    text: {
+      primary: "#212121",
+      secondary: "#6B6B6B",
+    },
+  },
 
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
 
-    
-  // palette: {
-  //   primary: {
-  //     main: "#259687",     //base color
-  //     light: "#4ebda9",
-  //     dark: "#1b786d",
-  //     contrastText: "#fff"
-  //   },
-  //   secondary: {
-  //     main: "#FF8A65",     // secondary color
-  //     contrastText: "#000",
-  //   },
-  //   background: {
-  //     //default: "#F5F7FA", 
-  //     paper: "#ffffff"
-  //   },
-  //   text: {
-  //     primary: "#212121",
-  //     secondary: "#6B6B6B",
-  //   },
-  // },
-
-  // typography: {
-  //   fontFamily: "Roboto, Arial, sans-serif",
-  //   h1: {
-  //     fontSize: "2.25rem",
-  //     fontWeight: 700,
-  //   },
-  //   h2: {
-  //     fontSize: "2rem",
-  //     fontWeight: 600,
-  //   },
-  //   body1: {
-  //     fontSize: "1rem",
-  //     fontWeight: 400,
-  //   },
-  //   button: {
-  //     textTransform: "none",
-  //     fontWeight: 600,
-  //   },
-  // },
-
-  // components: {
-  //   MuiButton: {
-  //     styleOverrides: {
-  //       root: {
-  //         borderRadius: "4px",
-  //         padding: "12px 20px",
-  //       },
-  //     },
-  //   },
-  //   MuiTextField: {
-  //     defaultProps: {
-  //       variant: "outlined",
-  //     },
-  //   },
-  // },
-
-  // breakpoints: {
-  //   values: {
-  //     xs: 0,
-  //     sm: 600,
-  //     md: 900,
-  //     lg: 1200,
-  //     xl: 1536,
-  //   },
-  // }
-
-
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "4px",
+        },
+      },
+    },
+  },
 };
 
 const theme = createTheme({
